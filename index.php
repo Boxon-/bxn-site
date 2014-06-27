be77fdbe5d3d75e68d33e374bd277ece61d1b3c1
baladurzgate <bighal.cormier@gmail.com>
2014-06-27 23:54:18 +0200

    erreur html

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <body>
 	<div id="main">
 		<div id="banniere"><?php include('Templates/banniere.html')?></div>
+		</br>
 		<div id="menuH"><?php include('Templates/menuH.html')?></div>
+		</br>
 		<div id="contenu"><?php include('Templates/contenu.html')?></div>
+		</br>
 	</div>
 <script>
 </script>
