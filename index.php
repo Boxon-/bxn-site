dfa86ccf03b033c6d8d68094fc7c20a2940a6e42
baladurzgate <bighal.cormier@gmail.com>
2014-06-27 23:31:11 +0200

    avec bannieres menuH et contenu

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php include('Templates/header.php')?>
 <body>
 	<div id="main">
+		<div id="banniere"><?php include('Templates/banniere.php')?></div>
+		<div id="menuH"><?php include('Templates/menuH.php')?></div>
+		<div id="contenu"><?php include('Templates/contenu.php')?></div>
 	</div>
 <script>
-	alert ('carabistouille!');
 </script>
 </body>
 <?php include('Templates/footer.php')?>
