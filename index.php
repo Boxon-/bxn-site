cf12425682261ef4cdb0ebbff73848e726a841e4
baladurzgate <bighal.cormier@gmail.com>
2014-06-27 22:44:46 +0200

    index avec les bons includes

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php include('template/header.php')?>
+<?php include('templates/header.php')?>
 <body>
 	<div id="main">
 	</div>
 <script>
 </script>
 </body>
-<?php include('template/footer.php')?>
+<?php include('templates/footer.php')?>
