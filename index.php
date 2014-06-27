ccecf52364829bc283f06e9cdeee50e0ea226d56
baladurzgate <bighal.cormier@gmail.com>
2014-06-28 01:20:34 +0200

    index+php GET_chapitre

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
-<?php include('Templates/header.php')?>
+<?php 
+// on reccupere le chapitre voulu et on l'affiche 
+$chapitre = 'accueil' ; 
+if($_GET['chapitre']&&$_GET['chapitre']!=""){
+	$chapitreActuel= htmlspecialchars($_GET['chapitre'], ENT_QUOTES); //on filtre le GET
+}
+include('Templates/header.php')?>
 <body>
 	<div id="main">
 		<div id="banniere"><?php include('Templates/banniere.html')?></div>
 		</br>
 		<div id="menuH"><?php include('Templates/menuH.html')?></div>
 		</br>
-		<div id="contenu"><?php include('Templates/contenu.html')?></div>
+		<div id="chapitre">
+		<?php
+			echo $chapitreActuel; 
+			include("Chapitres/".$chapitreActuel."/".$chapitreActuel.".html");
+		?>
+		</div>
 		</br>
 	</div>
 <script>
+// script de la page 
 </script>
 </body>
 <?php include('Templates/footer.php')?>
