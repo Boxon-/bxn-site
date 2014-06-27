5f86b4865efbdac2467794de5fe5aca2e5d2473f
baladurzgate <bighal.cormier@gmail.com>
2014-06-27 22:42:31 +0200

    dossier template

---
+++ Templates/header.php
@@ -0,0 +1,17 @@
+<html lang="fr">
+<head>
+<title>TEST</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="shortcut icon" href="" />
+<link rel="icon" type="image/x-icon" href="" />
+<link rel="icon" type="image/png" href="" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="geo.placename" content="Paris, France" />
+<link rel="apple-touch-icon" href="" />
+<meta name="viewport" content="width=1400 initial-scale=1.0" />
+<link rel="stylesheet" type="text/css" href="Templates/style1.css" />
+<script>
+	alert ('salut les gars!');
+ </script>
+</head>
