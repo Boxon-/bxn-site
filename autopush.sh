fbed848fa246d28b5e96d6558de88da032c27666
xavierKlein <xkleink@gmail.com>
2014-06-28 12:13:48 +0200

    Script pour auto-pusher vos fichiers favoris+voix du futr

--- autopush.sh
+++ autopush.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+espeak "What is the name of your file ?"
+
 echo "Fichier ?"
 read ADD
 
 git add $ADD
 
+espeak "Please, submit me a commit name or I rape you in the ass"
 echo "Nom du commit"
 read COMMIT
 
@@ -12,7 +15,8 @@ git commit -m "$COMMIT"
 
 git push
 
-espeak "Push completed. Well done, now you can kiss Mister Klein in the ass."
+espeak "Push completed. Well done, now you can continue your senseless human life."
+espeak "I'm your computer. I'm your best friend. Kill the other human being."
 
 exit 0
 
