d7947e7fa7da53e98c35bcfc0f64ca5700963503
Alexandre <bighal.cormier@gmail.com>
2014-06-28 19:09:22 +0200

    Update index.php
    raccord avec les classes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 include_once('init.php');
-include_once('HEAD.php')?>
+include_once('HEAD.php');?>
 	<div id="header"><!--marge superieure--><?php include_once("Templates/header.php");?></div>
 	<div id="main">
 		<div id="banniere"><!--Templates/banniere.html--><?php include_once('Templates/banniere.html')?></div>
@@ -8,10 +8,7 @@ include_once('HEAD.php')?>
 		<div id="menuH"><!--Templates/menuH.html--><?php include_once('Templates/menuH.html')?></div>
 		</br>
 		<div id="chapitre">
-			<?php
-				echo "<!--Chapitres/".$chapitreActuel."/".$chapitreActuel.".php--></br>"; 
-				include_once("Chapitres/".$chapitreActuel."/".$chapitreActuel.".php");
-			?>
+			<?php $chapitre->includeChapitre(); ?>
 		</div>
 	</div>
 	<div id="footer"><!--marge inferieure--><?php include_once("Templates/footer.php");?></div>
