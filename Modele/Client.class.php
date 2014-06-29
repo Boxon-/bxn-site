1787bbf8b6c7ed753221e2473b683c45e857b86d
Alexandre <bighal.cormier@gmail.com>
2014-06-29 23:50:35 +0200

    est maintenant un enfant de la classe Component

--- Modele/Client.class.php
+++ Modele/Client.class.php
@@ -1,30 +1,13 @@
 <?php
-class Client {
+class Client extends Component{
 	private $id;
 	private $site;
 	private $browser;
     function __construct(){
+		parent::__construct("client");
 		// recuperation des infos sur le navigateur du client
 		$this->browser = $_SERVER['HTTP_USER_AGENT'];
     }
-	public function __get($property) {
-		if (property_exists($this, $property)) {
-		  return $this->$property;
-		}
-	}
-
-	public function __set($property, $value) {
-		if (property_exists($this, $property)) {
-			switch($property){
-				case "browser":
-					$this->$property = $value;
-				break;			
-				case "id":
-					$this->$property = $value;
-				break;					
-			}
-		}
-	}
 	public function getBrowser(){
 		echo $_SERVER['HTTP_USER_AGENT'] . "\n\n";
 		$browser = get_browser(null, true);
