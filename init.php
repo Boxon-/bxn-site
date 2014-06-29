4dd9634e74293d6ffb78f1e3689358cc22ae6634
Alexandre <bighal.cormier@gmail.com>
2014-06-29 22:27:44 +0200

    ajouts des chemins absolus
    Utiliser des chemins absolus serveur ("S") permet d'inclure n'importe quel fichier du site dans un script php situé n'importe où sur le serveur sans avoir à détailler toute l'arboraissance. exemple : au lieu d'ecrire : require("../../Modele/lapin.php") on écris require($S->paths["S"]['Modele'].'lapin.php') qui renvoi une adresse depuis le serveur ici en local :"C:/wamp/www/ToDoList/Modele/lapin.php"

--- init.php
+++ init.php
@@ -1,9 +1,49 @@
 <?php
-	//classe mère
-	include_once("Modele/Site.class.php");	
+	// recuperation des infos sur le serveur
+	$serverName = $_SERVER['SERVER_NAME'];
+	$serverSoftware = $_SERVER['SERVER_SOFTWARE'];
+	$serverRoot = $_SERVER['DOCUMENT_ROOT'];
+	$serverSelf = $_SERVER['PHP_SELF'];
+	$fileURL=str_replace("index.php","",$serverSelf);
 	
-	//construction du site
-	$S=new Site('ToDoList');
+	//la variable root renvoi l'adresse absolue du site sur le serveur et url l'adresse relative pour le navigateur
+	$root;
+	$url;
+	if($serverName=='localhost'){
+		//si le site est lance en local 
+		$root = dirname(__FILE__).'/';
+		$url = "http://".$serverName.'/'.$fileURL;
+		
+	}else{
+		//depuis le serveur
+		$root = dirname(__FILE__);
+		$url = "http://".$serverName.$fileURL;
+	}
+	
+	//Importation de la classe Mere "Site"
+	require($root.'/Modele/Site.class.php');
+	
+	//construction de l'objet singleton $S issue de la classe Site
+	$S=Site::getInstance();
+	$S->name="ToDoList";
+	$S->ROOT = $root;
+	$S->URL = $url;
+	
+	//ajout des chemins detailles
+	//chemins serveur :
+	$S->addPath("S","Modele",$root.'/Modele/'); 
+	$S->addPath("S","Templates",$root.'/Templates/');
+	//urls :
+	$S->addPath("U","Modele",$url.'/Modele/');
+	$S->addPath("U","Templates",$url.'/Templates/');
+	
+	//Importation des autres classes
+	require($S->paths["S"]['Modele'].'Chapitre.class.php');
+	require($S->paths["S"]['Modele'].'Client.class.php');
+	require($S->paths["S"]['Modele'].'File.class.php');
+	require($S->paths["S"]['Modele'].'HTMLhandler.class.php');
+	require($S->paths["S"]['Modele'].'simple_html_dom.php');	
+	
+	//initialisation de l'objet Site
 	$S->init();
-	$chapitre = $S->chapitreActuel;
 ?>
