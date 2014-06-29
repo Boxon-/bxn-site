30857da671ca4969095d1da4cce5571d0bf153d8
Alexandre <bighal.cormier@gmail.com>
2014-06-29 23:52:35 +0200

    est maintenant enfant de la classe Component

--- Modele/Chapitre.class.php
+++ Modele/Chapitre.class.php
@@ -1,34 +1,14 @@
 <?php
-class Chapitre {
+class Chapitre extends Component {
 	private $name;
 	private $pathFromServer;
 	private $pathFromIndex;
 
     function __construct($name) {
+		parent::__construct("chapitre");
 		$this->name=$name;
-		$this->pathFromIndex="Chapitres/".$this->name."/".$this->name.".php";
+		$this->pathFromIndex=$this->S->paths["U"]["Chapitres"].$this->name."/".$this->name.".php";
     }
-	public function __get($property) {
-		if (property_exists($this, $property)) {
-		  return $this->$property;
-		}
-	}
-
-	public function __set($property, $value) {
-		if (property_exists($this, $property)) {
-			switch($property){
-				case "name":
-					$this->$property = $value;
-				break;
-				case "pathFromIndex":
-					$this->$property = $value;
-				break;
-				case "pathFromServer":
-					$this->$property = $value;
-				break;				
-			}
-		}
-	}
 	public function includeChapitre(){
 		echo "<!--".$this->pathFromIndex."--></br>"; 
 		include_once($this->pathFromIndex);	
