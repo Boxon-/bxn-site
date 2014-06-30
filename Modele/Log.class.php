79a86d76394be42915048f89d8618ef2912a7344
Alexandre <bighal.cormier@gmail.com>
2014-06-30 18:12:42 +0200

    correction de bug et commentaires
    suppression de la boucle infinie -> addTolog -> updateFile -> isAllowed -> addTolog 
    via la variable boolean $updateLog (true par default) qui permet de ne pas updater le log quand il s'agis de modification sur le log lui meme.

--- Modele/Log.class.php
+++ Modele/Log.class.php
@@ -1,14 +1,19 @@
 <?php
 class Log extends Component {
+	private $url;
 	private $file;
 	private $JsConsole;
 	private $listOfAlert;
-	private $listOfAlert;
 
-    function __construct($name) {
+    function __construct() {
 		parent::__construct("log");
-		$url = $this->S->paths['S']['Log'].'Log.txt';
-		$this->file =  new File($url);
+		//on creer un nouveau fichier Log.txt dans Log/
+		$this->isRelatedToLog=true;
+		
+		$url = $this->S->paths['S']['Log'].'/Log.html';
+		$this->file =  new File($url,false);
+		$this->file->isRelatedToLog=true;
+		$this->file->create(false);
 		$this->listOfAlert = array();
 		$this->JsConsole = "";
 		$this->updateFile();
@@ -19,34 +24,42 @@ class Log extends Component {
 		//$alert doit etre de type string
 		if(gettype($alert)=='string'){
 			//on ajoute l alerte a la liste
-			array_push($this->listOfAlert,$alert)
+			array_push($this->listOfAlert,$alert);
 			$this->Log.=$alert;
 			$this->JsConsole.="console.log('".$alert."');";
 		}
 	}
 	// actualise log.txt 
 	public function updateFile(){
-		$toWrite="";
-		foreach($listOfArray as $alert){
-			$toWrite.=$alert;
+		if(gettype($this->listOfAlert)=='array'){
+			$toWrite="<html><body>";
+			foreach($this->listOfAlert as $alert){
+				$toWrite.="<span>".$alert."</span></br>";
+			}
+			$toWrite.="</html></body>";
+			if($this->file->write($toWrite,'over',false)){
+				return true;
+			}
 		}
-		if($this->file->write($toWrite,'over')){
-			return true;
-		}
+		
 		return false;
 	}
 	// permet d'afficher le log dans la console js de firefox (necessite de mette "<script> <?php echo $S->Log->getJsConsole();> </script>" dans <head>)
 	public function updateJsConsole(){
 		$toJsConsole="";
-		foreach($listOfArray as $alert){
-			$toJsConsole.="console.log('".$alert."');";
+		if(gettype($this->listOfAlert)=='array'){
+			foreach($this->listOfAlert as $alert){
+				$toJsConsole.="console.log('".$alert."');";
+			}
 		}
 		$this->JsConsole = $toJsConsole;
 	}
 	
 	//renvoi la string JsConsole
 	public function getJsConsole(){
-		return $this->JsConsole = $toJsConsole;
+		return $this->JsConsole;
 	}
+
+
 }
 ?>
