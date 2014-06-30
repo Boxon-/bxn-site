1bd2d1b89bea33b857c6352ab5bdc62cf6666a76
Alexandre <bighal.cormier@gmail.com>
2014-06-30 18:26:48 +0200

    ajout alertes log

--- Modele/Client.class.php
+++ Modele/Client.class.php
@@ -1,12 +1,16 @@
 <?php
 class Client extends Component{
 	private $id;
-	private $site;
 	private $browser;
-    function __construct(){
+    public function __construct($updateLog=true){
 		parent::__construct("client");
 		// recuperation des infos sur le navigateur du client
 		$this->browser = $_SERVER['HTTP_USER_AGENT'];
+		//log
+		if($updateLog){
+			$this->Log("new Client ");
+			$this->Log("browser :".$this->browser);
+		}
     }
 	public function getBrowser(){
 		echo $_SERVER['HTTP_USER_AGENT'] . "\n\n";
