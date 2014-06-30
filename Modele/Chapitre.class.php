a9081842ba47b9598a900dd2d98e0e348bc8a896
Alexandre <bighal.cormier@gmail.com>
2014-06-30 18:23:35 +0200

    ajout alertes log

--- Modele/Chapitre.class.php
+++ Modele/Chapitre.class.php
@@ -3,12 +3,16 @@ class Chapitre extends Component {
 	private $name;
 	private $pathFromServer;
 	private $pathFromIndex;
-
-    function __construct($name) {
+	
+	public function __construct($name,$updateLog=true) {
 		parent::__construct("chapitre");
 		$this->name=$name;
 		$this->pathFromIndex=$this->S->paths["U"]["Chapitres"].$this->name."/".$this->name.".php";
-    }
+		if($updateLog){
+			$this->Log("new Chapitre :".$this->name);
+			$this->Log("pathFromIndex :".$this->pathFromIndex);
+		}
+	}
 	public function includeChapitre(){
 		echo "<!--".$this->pathFromIndex."--></br>"; 
 		include_once($this->pathFromIndex);	
