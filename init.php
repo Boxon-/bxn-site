4e36f4b88115296a5280a91bafef3c28de53c99c
Alexandre <bighal.cormier@gmail.com>
2014-07-01 05:39:11 +0200

    -de php

--- init.php
+++ init.php
@@ -1,27 +1,4 @@
 <?php
-	//desactivation des erreurs php
-	//error_reporting(0);	
-
-	// recuperation des infos sur le serveur
-	$serverName = $_SERVER['SERVER_NAME'];
-	$serverSoftware = $_SERVER['SERVER_SOFTWARE'];
-	$serverRoot = $_SERVER['DOCUMENT_ROOT'];
-	$serverSelf = $_SERVER['PHP_SELF'];
-	$fileURL=str_replace("index.php","",$serverSelf);
-	
-	//la variable root renvoi l'adresse absolue du site sur le serveur et url l'adresse relative pour le navigateur
-	$root;
-	$url;
-	if($serverName=='localhost'){
-		$root = dirname(__FILE__);
-		$url = "http://".$serverName.'/'.$fileURL;
-		
-	}else{
-		$root = dirname(__FILE__);
-		$url = "http://".$serverName.$fileURL;
-	}
-	
-
 	//Importation de la classe Mere
 	require($root.'/Modele/Site.class.php');
 	
