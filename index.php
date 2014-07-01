1e2fddcb3a76c2705e10c8a819c4fd80c9aa7ce0
Alexandre <bighal.cormier@gmail.com>
2014-07-01 05:38:00 +0200

    +++php
    deplacement d'une partie du code de init.php vers l'index

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php 
-include_once('init.php');
-include_once('HEAD.php');?>
+	//desactivation des erreurs php
+	//error_reporting(0);	
+
+	// recuperation des infos sur le serveur
+	$serverName = $_SERVER['SERVER_NAME'];
+	$serverSoftware = $_SERVER['SERVER_SOFTWARE'];
+	$serverRoot = $_SERVER['DOCUMENT_ROOT'];
+	$serverSelf = $_SERVER['PHP_SELF'];
+	$fileURL=str_replace("index.php","",$serverSelf);
+
+	//la variable root renvoi l'adresse absolue du site sur le serveur et url l'adresse relative pour le navigateur
+	$root;
+	$url;
+	if($serverName=='localhost'){
+		$root = dirname(__FILE__);
+		$url = "http://".$serverName.'/'.$fileURL;
+		
+	}else{
+		$root = dirname(__FILE__);
+		$url = "http://".$serverName.$fileURL;
+	}
+	require($root.'/init.php');
+	require($root.'/HEAD.php');
+?>
 	<div id="header"><!--marge superieure--><?php include_once("Templates/header.php");?></div>
 	<div id="main">
 		<div id="banniere"><!--Templates/banniere.html--><?php include_once('Templates/banniere.html')?></div>
@@ -15,4 +37,4 @@ include_once('HEAD.php');?>
 	<script>
 	// script de la page 
 	</script>
-<?php include_once('FOOT.php')?>
+<?php require($root.'/FOOT.php')?>
