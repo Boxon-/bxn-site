dddfa668f72417fac3b1db4321ba79c953141d73
Alexandre <bighal.cormier@gmail.com>
2014-08-16 18:01:52 +0200

    correction recouvrement sidebar

--- blog/wp-content/themes/blog/category.php
+++ blog/wp-content/themes/blog/category.php
@@ -1,8 +1,12 @@
-<?php get_header(); ?>
-<div class="contenu2">
-  <?php get_sidebar(); ?>
-  <p class="titrecontenu"><?php single_cat_title(); ?></p>
-  <?php get_template_part('loop'); ?>
+<?php 
+	get_header(); 
+	get_sidebar(); 
+?>
+<p class="titrecontenu"><?php single_cat_title(); ?></p>
+<div id="contenu" class="js-masonry" data-masonry-options='{ "columnWidth": 30, "itemSelector": ".item" }'>
+ <?php get_template_part('loop'); ?>
 </div>
+
 <?php get_footer(); ?>
-<?php include 'pad.php'; ?>
+<?php //include 'pad.php'; ?>
+
